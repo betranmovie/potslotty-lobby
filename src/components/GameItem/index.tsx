213ed776f57e4b2317f2f755de7014cf6f83e71f
betranmovie <trivien99tk@gmail.com>
2023-07-26 19:09:30 +0700

    updatge

--- src/components/GameItem/index.tsx
+++ src/components/GameItem/index.tsx
@@ -7,9 +7,9 @@ interface Props{
 export default function GameItem({gameInfo}:Props) {
 
     return (
-        <div className='flex-none mr-5'>
+        <div className='flex-none mr-5 w-28'>
             <Link href={`/play/${gameInfo.alias}`} >
-                <img className='md:h-48 h-28 rounded-xl cursor-pointer'
+                <img className='md:h-48 h-28 rounded-xl cursor-pointer object-cover	'
                     src={gameInfo.thumbnail}
                     id={gameInfo.name}
                 />
