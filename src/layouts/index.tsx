8430b30d8848ab46fd186474f03ade776f7ce832
betranmovie <trivien99tk@gmail.com>
2023-07-28 16:55:49 +0700

    update ui

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {/* <Footer/> */}
         </>
       </div>
-      <FooterNav />
+      {/* <FooterNav /> */}
     </>
   )
 }
