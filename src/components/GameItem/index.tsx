f7fccc265c62b5f9737038b90dcb2d40da637172
betranmovie <trivien99tk@gmail.com>
2023-07-29 11:36:41 +0700

    update no image

--- src/components/GameItem/index.tsx
+++ src/components/GameItem/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import NoImage from '@/assets/images/NoImage.png'
 
 interface Props{
     gameInfo: any
@@ -12,6 +13,10 @@ export default function GameItem({gameInfo}:Props) {
                 <img className='md:h-48 h-28 rounded-xl cursor-pointer object-cover	'
                     src={gameInfo.thumbnail}
                     id={gameInfo.name}
+                    onError={({ currentTarget }) => {
+                        currentTarget.onerror = null; // prevents looping
+                        currentTarget.src = NoImage.src
+                    }}
                 />
             </Link>
         </div>
